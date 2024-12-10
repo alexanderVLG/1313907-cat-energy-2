e8de078bd112a63cf36790e19665c1ebb7d42364
alexanderVLG <40559899+alexanderVLG@users.noreply.github.com>
2024-12-10 16:42:10 +0000

    Deploying to gh-pages from @ alexanderVLG/1313907-cat-energy-2@2759062562b4b2bf589df1718d4676f5a9057b75 🚀

--- 19/scripts/index.js
+++
@@ -1,1 +0,0 @@
-var t=document.querySelector(".button-wrapper"),e=document.querySelector(".burger-button");t.addEventListener("click",()=>{e.classList.toggle("burger-button--show")});
---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+var t=document.querySelector(".button-wrapper"),e=document.querySelector(".burger-button");t.addEventListener("click",()=>{e.classList.toggle("burger-button--show")});
